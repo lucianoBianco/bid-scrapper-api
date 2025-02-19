d749475054964b0809a60f2e1056aa796e3bcc94
lucianobianco <luciano.bianco@ebmgroup.com.br>
2025-02-19 16:59:46 -0300

    setup scripts added

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Atualizar pacotes e instalar dependências
+sudo apt update
+sudo apt install -y python3-pip python3-dev python3-venv nginx
+
+# Criar e ativar ambiente virtual
+python3 -m venv /home/your_user/api/bid-scrapper-api/venv
+source /home/your_user/api/bid-scrapper-api/venv/bin/activate
+
+# Instalar dependências do projeto
+pip install --upgrade pip
+pip install flask gunicorn selenium beautifulsoup4
+
+# Baixar o ChromeDriver (ajuste conforme necessário, dependendo da versão do Chrome)
+wget https://chromedriver.storage.googleapis.com/116.0.5845.96/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+
+# Garantir permissões adequadas para o ChromeDriver
+sudo chmod +x /usr/local/bin/chromedriver
+
+# Configuração do Nginx e Gunicorn será feita manualmente a seguir
